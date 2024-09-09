a354c173fe984e4138bdeaf3d82117b38a631ea6
mitty1293 <k.fmichi@gmail.com>
2024-09-09 22:26:41 +0900

    5.5まで終わり。次回は5.6から。

--- reffect-sveltekit-first/src/routes/todo/+page.server.ts
+++ reffect-sveltekit-first/src/routes/todo/+page.server.ts
@@ -1,5 +1,22 @@
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad, Actions } from "./$types";
 import { prisma } from "$lib/db";
+
+export const actions = {
+    create: async ({ request }) => {
+        console.log("create");
+        const data = await request.formData();
+        console.log(data);
+    },
+    delete: async ({ request }) => {
+        const data = await request.formData();
+        const id = Number(data.get("id"));
+        await prisma.todo.delete({
+            where: {
+                id
+            }
+        });
+    }
+} satisfies Actions;
 
 export const load = (async () => {
     const todos = await prisma.todo.findMany();
--- reffect-sveltekit-first/src/routes/todo/+page.svelte
+++ reffect-sveltekit-first/src/routes/todo/+page.svelte
@@ -4,8 +4,23 @@
 </script>
 
 <h1>Todo</h1>
+
+<form method="post" action="?/create">
+    <div>
+        <label for="name">Name:</label>
+        <input name="name" id="name">
+    </div>
+    <button  type="submit">Add</button>
+</form>
+
 <ul>
     {#each data.todos as todo}
-        <li>{todo.name}</li>
+        <li style="display:flex;">
+            <span>{todo.name}</span>
+            <form method="post" action="?/delete">
+                <input type="hidden" name="id" value={todo.id}>
+                <button>X</button>
+            </form>
+        </li>
     {/each}
 </ul>
